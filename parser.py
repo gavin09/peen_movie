1bc20c6d78fadc0c34789dde7631f70ae1feba05
Gavin Lai <gavin@GavinLais-MacBook-Pro.local>
2015-04-10 01:08:21 +0800

    support export parsing results

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
 import urllib2
+import json
 from bs4 import BeautifulSoup
 
 class Parser:
     def __init__(self):
-        self.test = 0
+        self.raw_data = []
 
     def get_article(self, url):
         req = urllib2.urlopen(url)
@@ -15,7 +16,7 @@ class Parser:
                 url = article.a.get('href')
                 title = article.a.contents[0]
                 title_utf8 = title.encode('utf-8')
-                print title_utf8
+                self.raw_data.append((url, title_utf8))
 
     def get_previous_page_url(self, url):
         req = urllib2.urlopen(url)
@@ -36,6 +37,8 @@ if __name__ == '__main__':
     url = start_url
     while True:
         parser.get_article(url)
+        with open('raw.txt', 'w') as outfile:
+            json.dump(parser.raw_data, outfile)
         next_url = parser.get_previous_page_url(url)
         if next_url == None:
             break
